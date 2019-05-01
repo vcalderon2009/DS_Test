070de3deddecd44b39efb8300775fb9f071a536a
Victor Calderon <victor.calderon90@gmail.com>
2019-05-01 02:00:04 -0500

    Updated 'question_2'

--- src/ds_victor_calderon_solutions.py
+++ src/ds_victor_calderon_solutions.py
@@ -104,7 +104,7 @@ def question_1(df, ntop=10):
 # For each facility ("Provider.Name"), which DRGs form the largest portion
 # of the patient population?
 
-def question_2(df):
+def question_2(df, return_all=False, ntop=10, return_type='dict'):
     """
     This function determines which DRGs form the largest portion of the
     patient population for each facility.
@@ -114,11 +114,83 @@ def question_2(df):
     df : `pandas.DataFrame`
         DataFrame containing the original information about DRG.
 
+    return_all : `bool`, optional
+        If `True`, the function returns the complete dictionary for all of
+        the facilities that includes ``all`` of the different DRGs treated
+        at each facilities. This variable is set to `False` by default.
+
+    ntop : `int`, optional
+        Number of top DRGs to return. This variable is only used when
+        ``return_all = False``. Otherwise, it is not used.
+        This variable is set to ``10`` by default.
+
+    return_type : {`pd`, `dict`}, optional
+        Option for which type of element to return. This variable is set
+        to `dict` by default.
+
+        Options:
+            - `dict` : A dictionary is returned with info on DRGs for each provider
+            - `pd` : A DataFrame is returned with info on DRGs for each provider
+
     Returns
     ---------
-    prov_dict : `dict`
-        Dictionary containing 
+    return_obj : {`dict`, `pandas.DataFrame`}
+        Python dictionary or `pandas.DataFrame` with the information on
+        DRGs for each of the different facilities. This variable depends
+        on the value of `return_type`.
     """
+    # Columns to keep
+    disch_col = 'total_discharges'
+    prov_col  = 'provider_name'
+    drg_col   = 'drg_definition'
+    cols_keep = [prov_col, disch_col, drg_col]
+    # Creating sub-DataFrame
+    prov_main_df = df.loc[:, cols_keep]
+    # Unique set of providers
+    prov_unq  = prov_main_df[prov_col].unique()
+    # Unique facilities
+    print('There are `{0}` unique facilities'.format(prov_unq.shape[0]))
+    # Saving output file as dictionary
+    # Initializing dictionary
+    prov_dict = {}
+    # Looping over each unique facility and sorting DRGs by number of patients
+    tqdm_msg = 'Unique providers: '
+    for prov_ii in tqdm(prov_unq, desc=tqdm_msg):
+        if return_all:
+            prov_dict[prov_ii] = (prov_main_df.loc[
+                prov_main_df[prov_col] == prov_ii]
+                .sort_values(by=disch_col, ascending=False)
+                .reset_index(drop=True)
+                .drop(prov_col, axis=1))
+        else:
+            prov_dict[prov_ii] = (prov_main_df.loc[
+                prov_main_df[prov_col] == prov_ii]
+                .sort_values(by=disch_col, ascending=False)
+                .reset_index(drop=True)
+                .drop(prov_col, axis=1)).head(ntop)
+    # Creating DataFrame with the most common DRG per facility
+    if (return_type == 'pd'):
+        # Grouping by facilities and selecting the DRG with largest
+        # number of discharge
+        prov_grouped = [[] for x in range(prov_unq.shape[0])]
+        # Looping over all unique facilities
+        for ii, prov_ii in enumerate(tqdm(prov_unq)):
+            prov_ii_pd = prov_dict[prov_ii].head(1)
+            prov_grouped[ii] = prov_dict[prov_ii].head(1)
+        # Concatenating list
+        prov_max_grouped = pd.concat(prov_grouped).reset_index(drop=True)
+        # Adding column
+        prov_max_grouped.loc[:, prov_col] = prov_unq
+        # Rearranging column order
+        prov_max_grouped = (prov_max_grouped[[prov_col, drg_col, disch_col]]
+                            .sort_values(prov_col).reset_index(drop=True))
+
+    if (return_type == 'dict'):
+        return_obj = prov_dict
+    elif (return_type == 'pd'):
+        return_obj = prov_max_grouped
+
+    return return_obj
 
 
 
@@ -160,7 +232,9 @@ def main(args):
     # Extracting and cleaning data
     df = data_extract_clean()
     # Question 1
+    question_1(df)
     # Question 2
+    prov_obj = question_2(df, return_type='pd')
     # Question 3
     # Question 4
 
